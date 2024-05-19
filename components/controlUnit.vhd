14b33a4dce90a7cc382aade510f0d07d8f56bbca
João Lanzarini <joaoandrelanzarini@gmail.com>
2024-05-19 13:38:49 -0300

    19/05

--- components/controlUnit.vhd
+++ components/controlUnit.vhd
@@ -4,17 +4,15 @@ use ieee.numeric_std.all;
 
 entity controlUnit is
     port(
-        clk, rst, zero : in std_logic;
-        instruction : in unsigned(15 downto 0);
+        clk, is_zero : in std_logic;
         addr : in unsigned(6 downto 0);
-        immediate : out unsigned(15 downto 0);
-        jump_en : out std_logic;
         next_addr : out unsigned(6 downto 0);
-        op_ula : out unsigned(1 downto 0);
-        write_en : out std_logic;
-        readReg1, readReg2 : out unsigned(2 downto 0);
-        writeReg : out unsigned(2 downto 0);
-        imm_op : out std_logic;
+        instruction : in unsigned(15 downto 0);
+        ext_imm : out unsigned(15 downto 0);
+        ula_op : out unsigned(1 downto 0);
+        RB_src : out std_logic;
+        acu_src : out unsigned(1 downto 0);
+        wr_reg, read_reg : out unsigned(2 downto 0);
         state_out : out unsigned(1 downto 0)
     );
 end entity controlUnit;
@@ -26,94 +24,34 @@ architecture a_controlUnit of controlUnit is
               state: out unsigned(1 downto 0)
         );
      end component;
-    
-    signal state : unsigned(1 downto 0) := "00";
-    signal opcode : unsigned(4 downto 0) := "00000";
-    signal imm : unsigned(15 downto 0) := "0000000000000000";
-    signal jump_enable : std_logic := '0';
 
-    begin
-        uut : StateMachine port map(clk => clk, rst => rst, state => state);
+     signal opcode : unsigned(3 downto 0);
+     signal imm : unsigned(8 downto 0);
+     signal ext_imm : unsigned(15 downto 0);
+ 
+     begin
+        uut : stateMachine port map(clk, rst, state);
 
-        opcode <= instruction(15 downto 11);
-        imm <=  "00000000000" & instruction(4 downto 0) when instruction(4) = '0' and opcode = "00111" else
-                "11111111111" & instruction(4 downto 0) when instruction(4) = '1' and opcode = "00111" else 
-                "00000000000" & instruction(4 downto 0) when opcode = "01000" else
-                (others => '0');
-        immediate <= imm;
-        imm_op <= '1' when opcode = "00010" or opcode = "00100" or opcode = "00111" or opcode = "01000" else '0';
-
-        jump_enable <= '1' when (opcode = "00111" and zero = '1') or opcode = "01000" else '0';
-        jump_en <= jump_enable;
-
-        next_addr <= addr + 1 when state = "01" and jump_enable = '0' else
-                       addr when state = "00" and jump_enable = '0' else
-                       imm(6 downto 0) when state = "01" and jump_enable = '1' and opcode = "01000" else
-                       addr + imm(6 downto 0) when state = "01" and jump_enable = '1' and opcode = "00111" else
-                       addr;
+        opcode <= instruction(15 downto 12);
+        imm <= instruction(8 downto 0);
+        ext_imm <= "0000000" & imm when opcode = "1010" or imm(8) = '0' else
+                    "1111111" & imm when imm(8) = '1';
         
-        state_out <= state;
-    process(opcode)
-    begin
-        case opcode is
-            when "00000" =>
-                op_ula <= "11";
-                write_en <= '0';
-                readReg1 <= "000";
-                readReg2 <= "000";
-                writeReg <= "000";
-            when "00001" =>
-                op_ula <= "11";
-                write_en <= '1';
-                readReg1 <= instruction(7 downto 5);
-                readReg2 <= "000";
-                writeReg <= instruction(10 downto 8);
-            when "00010" =>
-                op_ula <= "11";
-                write_en <= '1';
-                readReg1 <= "000";
-                readReg2 <= "000";
-                writeReg <= instruction(10 downto 8);
-            when "00011" =>
-                op_ula <= "00";
-                write_en <= '1';
-                readReg1 <= instruction(7 downto 5);
-                readReg2 <= "000";
-                writeReg <= "000";
-            when "00100" =>
-                op_ula <= "11";
-                write_en <= '1';
-                readReg1 <= "000";
-                readReg2 <= "000";
-                writeReg <= "000";
-            when "00101" =>
-                op_ula <= "01";
-                write_en <= '1';
-                readReg1 <= instruction(7 downto 5);
-                readReg2 <= "000";
-                writeReg <= "000";
-            when "00110" =>
-                op_ula <= "01";
-                write_en <= '0';
-                readReg1 <= instruction(7 downto 5);
-                readReg2 <= "000";
-                writeReg <= "000";
-            when "00111" =>
-                op_ula <= "01";
-                write_en <= '0';
-                readReg1 <= instruction(7 downto 5);
-                readReg2 <= "000";
-                writeReg <= "000";
-            when "01000" =>
-                op_ula <= "11";
-                write_en <= '0';
+        ula_op <= "00" when opcode = "0101" or opcode = "0110" else --ADD, ADDI
+                  "01" when opcode = "0111" or opcode = "1000" or opcode = "1001" else --SUB, CMP, JMP
+                  "11";
 
-            when others =>
-                op_ula <= "00";
-                write_en <= '0';
-                readReg1 <= "000";
-                readReg2 <= "000";
-                writeReg <= "000";
-        end case;
-    end process;
+        RB_src <= '1' when opcode = "0100" else --LI
+                  '0'; --MOV
+        
+        acu_src <= "10" when opcode = "0011" else
+                    "01" when opcode = "0001" else
+                    "00";
+
+        next_addr <= addr + 1 when state = "00" else
+                     addr when state = "01" else
+                     imm(6 downto 0) when opcode = "1010" else
+                     
+
+        state_out <= state;
 end architecture a_controlUnit;
